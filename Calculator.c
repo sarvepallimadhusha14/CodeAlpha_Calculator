555a45d1f3c8378b7845003160113776b87b50e8
sarvepallimadhusha14 <sarvepallimadhusha@gmail.com>
2026-06-15 14:55:31 +0530

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,39 @@
+# CodeAlpha Calculator Program
+
+## Description
+
+This project is a simple Calculator Program developed in C as part of the CodeAlpha Internship.
+
+The calculator performs basic arithmetic operations using a switch-case statement.
+
+## Features
+
+* Addition
+* Subtraction
+* Multiplication
+* Division
+* Menu-driven interface
+
+## Technologies Used
+
+* C Programming
+* GCC Compiler
+* Visual Studio Code
+
+## How to Run
+
+Compile:
+gcc calculator.c -o calculator
+
+Run:
+calculator
+
+## Learning Outcomes
+
+* Switch Case
+* Functions
+* User Input and Output
+
+## Author
+
+CodeAlpha Internship Project
